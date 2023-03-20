5a2c97ccc7964be145d40a55805d181f2b4945bd
Dennis J. Alberts <dennisalberts@hotmail.com>
2023-03-20 15:11:39 +0100

    Added subdirectories and examples

--- example_memory_allocation/rust/src/main.rs
+++ example_memory_allocation/rust/src/main.rs
@@ -1,7 +1,5 @@
 fn main() 
 {
-    let array: [i32; 10] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-    println!("Array[{}] = {}", 20, array[20]);
+    let mut ptr = Box::new(42);
 }
 
--- hello_world/cpp/main.cpp
+++ hello_world/cpp/main.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
-#include <thread>
 
 int main(int argc, char *argv[])
 {
-    int array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-    std::cout << "Array[" << 20 << "] = " << array[20] << std::endl; 
+    std::cout << "Hello world!" << std::endl; 
 }
 
 
