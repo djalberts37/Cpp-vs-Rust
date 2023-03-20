e3f46fe0af0b5b604a6b19890aade1db525edc73
Dennis J. Alberts <dennisalberts@hotmail.com>
2023-03-20 15:10:26 +0100

    Initial commit.

---
+++ hello_world/cpp/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <thread>
+
+int main(int argc, char *argv[])
+{
+    int array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    std::cout << "Array[" << 20 << "] = " << array[20] << std::endl; 
+}
+
+
