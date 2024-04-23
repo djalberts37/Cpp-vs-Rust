f8b4214ef703711397bb19a005a79310e77f0b05
Dennis J. Alberts <dennisalberts@hotmail.com>
2024-04-23 13:01:01 +0200

    Added fix of data races example to file in comments.

--- example_data_races/rust/src/main.rs
+++ example_data_races/rust/src/main.rs
@@ -26,3 +26,43 @@ fn main()
 
     println!("Shared_value = {}", shared_value.load(Ordering::SeqCst));
 }
+
+// use std::sync::{Arc, Mutex};
+// use std::thread;
+
+// fn main() 
+// {
+//     let shared_value = Arc::new(Mutex::new(0));
+
+//     let thread1 = 
+//     {
+//         let shared_value = shared_value.clone();
+//         thread::spawn(move || 
+//         {
+//             for _ in 0..1000000 
+//             {
+//                 let mut value = shared_value.lock().unwrap();
+//                 *value += 1;
+//             }
+//         })
+//     };
+
+//     let thread2 = 
+//     {
+//         let shared_value = shared_value.clone();
+//         thread::spawn(move || 
+//         {
+//             for _ in 0..1000000 
+//             {
+//                 let mut value = shared_value.lock().unwrap();
+//                 *value += 1;
+//             }
+//         })
+//     };
+
+//     thread1.join().unwrap();
+//     thread2.join().unwrap();
+
+//     let value = shared_value.lock().unwrap();
+//     println!("Shared_value = {}", *value);
+// }
